f4f46baed6642e5ffc4af5758951009b206f8bee
Keisuke Horikawa <keisuke.horikawa@gmail.com>
2023-05-23 13:14:16 +1000

    Initial commit

---
+++ ABC_151-200/AtCoder_ABC_167_A.py
@@ -0,0 +1,19 @@
+# https://atcoder.jp/contests/abc167/tasks/abc167_a
+# A - Registration
+
+import io
+import sys
+
+_INPUT = """\
+chokudai
+chokudaiz
+"""
+
+sys.stdin = io.StringIO(_INPUT)
+
+S = input()
+T = input()
+if S == T[0:-1]:
+    print("Yes")
+else:
+    print("No")
---
+++ ABC_151-200/AtCoder_ABC_167_B.py
@@ -0,0 +1,21 @@
+# https://atcoder.jp/contests/abc167/tasks/abc167_b
+# B - Easy Linear Programming
+
+import io
+import sys
+
+_INPUT = """\
+2000000000 0 0 2000000000
+"""
+
+sys.stdin = io.StringIO(_INPUT)
+
+A, B, C, K = map(int, input().split())
+
+if A >= K:
+    ans = K
+elif K-A <= B:
+    ans = A
+else:
+    ans = A- (K-A-B)
+print(ans)
